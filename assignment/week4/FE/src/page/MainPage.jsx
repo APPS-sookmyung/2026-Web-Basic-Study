88402d13a5509fa58067c8c1384a21831f03eb1d
임유진 <lyj5721@sookmyung.ac.kr>
2026-05-06 03:02:28 +0900

    Docs: 4주차 과제 관련 파일

---
+++ assignment/week4/FE/src/page/MainPage.jsx
@@ -0,0 +1,15 @@
+import Movie from "/src/components/Movie";
+import movieData from "/src/mock/dummy.json";
+
+function MainPage() {
+  return (
+    <div className="main-container">
+      <div className="movie-grid">
+        {/* 데이터 배열을 돌며 Movie 컴포넌트 생성 */}
+        {?}
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
