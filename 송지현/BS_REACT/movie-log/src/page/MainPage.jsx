72fb66cb890eafeab420008bb7cdd07f388af481
jihyun41 <jihyun418@sookmyung.ac.kr>
2026-05-07 10:57:22 +0900

    feat: React 과제

---
+++ 송지현/BS_REACT/movie-log/src/page/MainPage.jsx
@@ -0,0 +1,16 @@
+import Movie from "/src/components/Movie";
+import movieData from "/src/mock/dummy.json";
+
+function MainPage() {
+  return (
+    <div className="main-container">
+      <div className="movie-grid">
+        {movieData.map((movie) => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
---
+++ 송지현/BS_REACT/movie-log/src/page/MoviePage.jsx
@@ -0,0 +1,39 @@
+import { useParams } from "react-router-dom";
+import movieData from "../mock/dummy.json";
+import "./MoviePage.css";
+
+function MoviePage() {
+  const { id } = useParams();
+  const movie = movieData.find((m) => m.id === Number(id));
+
+  if (!movie) {
+    return <div className="error">영화를 찾을 수 없습니다.</div>;
+  }
+
+  return (
+    <div className="movie-detail-container">
+      {/* 상단: 큰 포스터 이미지 */}
+      <div className="detail-poster">
+        <img src={movie.posterImgUrl} alt={movie.title} />
+      </div>
+
+      {/* 하단: 영화 상세 정보 */}
+      <div className="detail-content">
+        <h1>{movie.title}</h1>
+        <p className="detail-subtitle">{movie.subTitle}</p>
+
+        <div className="detail-genres">
+           {movie.genres.map((genre,index) => (
+            <span key={index}>{genre}</span>
+          ))}
+        </div>
+
+        <div className="detail-summary">
+          <p>{movie.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MoviePage;
