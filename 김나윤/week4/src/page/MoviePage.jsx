aee345cb1ad25a457d45e187f450d20e874923c0
nayun0083 <luzdelsolnayun@gmail.com>
2026-05-11 16:13:17 +0900

    Feat: 4주차 과제

---
+++ 김나윤/week4/src/page/MoviePage.jsx
@@ -0,0 +1,40 @@
+import { useParams } from "react-router-dom";
+import movieData from "../mock/dummy.json";
+import "./MoviePage.css";
+
+function MoviePage() {
+
+  const { id } = useParams();
+
+  const movie = movieData.find((m) => m.id === Number(id));
+
+  
+  if (!movie) {
+    return <div className="error">영화를 찾을 수 없습니다.</div>;
+  }
+
+  return (
+    <div className="movie-detail-container">
+      <div className="detail-poster">
+        <img src={movie.posterImgUrl} alt={movie.title} />
+      </div>
+
+      <div className="detail-content">
+        <h1>{movie.title}</h1>
+        <p className="detail-subtitle">{movie.subTitle}</p>
+
+        <div className="detail-genres">
+          {movie.genres.map((genre) => (
+            <span key={genre}>{genre}</span>
+          ))}
+        </div>
+
+        <div className="detail-summary">
+          <p>{movie.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MoviePage;
