9e5a6c6231de189027fac25032cdd557eef5bafc
kbychloe2006 <kbychloe2006@naver.com>
2026-05-18 20:53:04 +0900

    Feat: 5주차 과제

---
+++ 구보윤/week5/racingcar/src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+import java.util.Random;
+
+public class RandomNumberGenerator implements NumberGenerator {
+    private static final Random random = new Random();
+
+    @Override
+    public int generate() {
+        return random.nextInt(10);
+    }
+}
